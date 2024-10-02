42a5a00f7c2ccea3a543aacdf25826525cecb3f4
almasansri16 <almashanif126@gmail.com>
2024-10-03 01:02:45 +0500

    Dashboards Work Complete

--- src/app/(dashboard)/parent/page.jsx
+++ src/app/(dashboard)/parent/page.jsx
@@ -2,13 +2,13 @@ import Announcement from "@/app/components/Calender/Announcement";
 import BigCalendar from "@/app/components/Calender/BigCalendar";
 import React from "react";
 
-export default function TeacherPage() {
+export default function ParentPage() {
   return (
     <div className="flex flex-1 p-4 gap-4 xl:flex-row">
       {/* Left */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white rounded-md">
-          <h1 className="text-x font-semibold p-4">Schedule</h1>
+          <h1 className="text-x font-semibold p-4">Schedule (Muhammad Salaar)</h1>
           <BigCalendar />
         </div>
       </div>
